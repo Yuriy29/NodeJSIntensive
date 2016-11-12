118a97052aef5955b94426ad923a721ea3a2cbbe
Yuriy29 <dopayura@gmail.com>
2016-11-12 22:37:15 +0200

    refactoring

--- task2B.js
+++ task2B.js
@@ -5,31 +5,20 @@ app.use(cors());
 app.get('/', function (req, res) {
   var result = "Invalid fullname";
   var inputString = req.query.fullname;
-  if(((inputString) !== undefined && (0 < inputString.length))){
-    var s = (inputString).toString();
-    var outPutArray = new Array();
-     var inputArray = s.split(' ');
+  var s = (inputString).toString();
+  var outPutArray = new Array();
+  var inputArray = s.split(' ');
+  if(((inputString) !== undefined && (0 < inputString.length && inputArray.length <=3))){
      for(var i = 0; i < inputArray.length; i++){
-       var j = inputArray.length;
-       if(inputArray.length > 3){
-         break;
-       }
        if(i == 1){
-         console.log(inputArray[0]);
           outPutArray[i] = inputArray[0].charAt(0)+".";
-       }else {
-         console.log(inputArray[j - 1]);
-         if(i == 2){
+       }else if(i == 2){
           outPutArray[i] = inputArray[1].charAt(0)+".";
         }else{
-          outPutArray[i] = inputArray[j - 1];
+          outPutArray[i] = inputArray[inputArray.length - 1];
         }
      }
-       j--;
-     }
-     if(inputArray.length <= 3){
-        result = outPutArray.join(" ");
-     }
+     result = outPutArray.join(" ");
   }
 
   res.send(result.toString());
